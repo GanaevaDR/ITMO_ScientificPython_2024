68a0e3ea369192f7ccdff8c268505f4fcfef26e6
Daria Ganaeva <ganaeva.dasha@gmail.com>
2024-05-08 17:52:14 +0300

    Fixed regex error and finished HW2_2

--- HW2/HW2_2.py
+++ HW2/HW2_2.py
@@ -1,4 +1,14 @@
+#installation
+
+! pip install biopython
+! pip install -q condacolab
+import condacolab
+condacolab.install()
+! conda install -c bioconda seqkit
+
+
 #import libraries
+
 import requests
 import json
 import re
@@ -6,12 +16,8 @@ import subprocess
 from Bio import SeqIO
 import json
 
+#Seqkit function
 
-# load the testing data
-file = "test_file.fasta"
-ext = "fasta"
-
-#Seqkit part
 def seqkit_parser(filename):
   seqkit = subprocess.run(("seqkit", "stats", filename, "-a"),
                             capture_output=True,
@@ -25,46 +31,45 @@ def seqkit_parser(filename):
 
     return fasta_stats_sk
   
-#parse using seqkit
-res = seqkit_parser(file)
-print("Output of seqkit_parser: ", res)
+#Biopython function
 
-#get type of sequences
-type_f = list(seqkit_parser(file).values())[1]
-print("The type of sequence is: ", type_f)
-
-#Biopython part
 def biopython_parser(filename):
   type_f = list(seqkit_parser(filename).values())[1]
 
   biopy = SeqIO.parse(filename, ext)
   ids= []
+  seqs = []
+  desc = []
 
   if type_f == "Protein":
     for seq in biopy:
-      s_match = re.fullmatch('[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}', seq.description)
-      ids.append(s_match)
+      s_match = re.search('[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}', seq.description)
+      if s_match !=None:
+        ids.append(s_match.group())
+        seqs.append(seq.seq)
+        desc.append(seq.description)
 
   elif type_f == "DNA":
     for seq in biopy:
-      s_match = re.fullmatch('ENS([A-Z]{0})(E|FM|G|GT|P|R|T)[0-9]{11}|ENS[A-Z]{3,4}(E|FM|G|GT|P|R|T)[0-9]{11}', seq.description)
-      ids.append(s_match)
+      s_match = re.search('ENS([A-Z]{0})(E|FM|G|GT|P|R|T)[0-9]{11}|ENS[A-Z]{3,4}(E|FM|G|GT|P|R|T)[0-9]{11}', seq.description)
+      if s_match !=None:
+        ids.append(s_match.group())
+        seqs.append(seq.seq)
+        desc.append(seq.description)
+  else:
+    return "no matches"
 
-  return ids
-
-#the code breaks at extracting IDs:
-res = biopython_parser(file)
-print(res) #[None, None]
-
-#Part from HW2_1
+  return ids, seqs, desc
 
 #function 1 for Uniprot
+
 def get_uniprot(ids: list):
   accessions = ','.join(ids)
   http_args = {'params': {'accessions': accessions}}
   return requests.get("https://rest.uniprot.org/uniprotkb/accessions", **http_args)
 
 #function 2 for Uniprot
+
 def parse_response_uniprot(resp: dict):
     resp = resp.json()
     resp = resp["results"]
@@ -74,11 +79,12 @@ def parse_response_uniprot(resp: dict):
         species = val['organism']['scientificName']
         gene = val['genes']
         seq = val['sequence']
-        output[acc] = {'organism':species, 'geneInfo':gene, 'sequenceInfo':seq, 'type':'protein', "========================": "========================"}
+        output[acc] = {'organism':species, 'geneInfo':gene, 'sequenceInfo':seq, 'type':'protein'}
 
     return output
 
 #function 1 for Ensembl
+
 def get_ensembl(ids: list):
   server = "https://rest.ensembl.org"
   ext = "/lookup/id"
@@ -86,7 +92,8 @@ def get_ensembl(ids: list):
   headers={ "Content-Type" : "application/json", "Accept" : "application/json"}
   return requests.post(server+ext, headers=headers, data=json.dumps(id_s))
 
-# function 2 for Ensembl
+#function 2 for Ensembl
+
 def parse_response_ensembl(resp: dict):
     resp = resp.json()
     output = {}
@@ -96,21 +103,65 @@ def parse_response_ensembl(resp: dict):
         description = val["description"]
         feature = val["biotype"]
         transcript = val["canonical_transcript"]
-        output[key] = {"organism": species, "gene": gene, "description": description, "feature": feature, "transcript name": transcript, "============": "============"}
+        output[key] = {"organism": species, "gene": gene, "description": description, "feature": feature, "transcript name": transcript}
 
     return output
 
-#PART 2
-#function for identifying the source by ID using regex and outputing corresponding parsed result
+#regex & API function
+
 def regex_fun(ids: list):
   if re.fullmatch('[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}', ids[0]):
-    resp = get_uniprot(my_ids)
+    resp = get_uniprot(ids)
     res = parse_response_uniprot(resp)
 
   elif re.fullmatch('ENS([A-Z]{0})(E|FM|G|GT|P|R|T)[0-9]{11}|ENS[A-Z]{3,4}(E|FM|G|GT|P|R|T)[0-9]{11}', ids[0]):
-    resp = get_ensembl(my_ids)
+    resp = get_ensembl(ids)
     res = parse_response_ensembl(resp)
 
   return res
 
+#final function for parsing fasta files
 
+def final_function(file):
+  seqkit_res = seqkit_parser(file)
+
+  type_f = list(seqkit_parser(file).values())[1]
+  if type_f == "Protein":
+    db = "Uniprot"
+  else:
+    db = "Ensembl"
+
+  print("Output of seqkit stats for the provided file: ")
+  print()
+
+  for i in range(len(seqkit_res)):
+    print(*list(seqkit_res.items())[i])
+
+
+  print()
+  print("The search will be performed against the following database: ", "\n", db)
+  print()
+
+
+  seq = biopython_parser(file)[1]
+  desc = biopython_parser(file)[2]
+  ids = biopython_parser(file)[0]
+  API_res = regex_fun(ids)
+
+  for i in range(len(ids)):
+    print("ID: ", ids[i])
+    print()
+    print("Description: ", desc[i])
+    print("Sequence: ", seq[i])
+    print("Output of API request: ", "\n", list(API_res.items())[i])
+    print()
+    print("=================")
+    print()
+
+#testing
+
+file = "your_file.fasta"
+ext = "fasta"
+
+res = final_function(file)
+print(res)
